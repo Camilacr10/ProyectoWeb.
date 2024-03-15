21fb10856000f892c9281c94f51ad3e40a17529f
Camilacr10 <130608400+Camilacr10@users.noreply.github.com>
2024-03-14 21:00:46 -0600

    Semana 9 (Practica 4)

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -4,6 +4,7 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data
@@ -22,6 +23,10 @@ public class Categoria implements Serializable { //serializacion porque se va al
     private String rutaImagen;
     private boolean activo;
 
+    @OneToMany
+    @JoinColumn(name="id_categoria", updatable=false)
+    List<Producto> productos;
+    
     public Categoria() {
     }
 
